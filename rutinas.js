952e840173174fd6b1bbc9f61d9cd9838ad6a888
Deiner Domínguez Bolaño <deiner_db@hotmail.com>
2017-07-16 20:45:20 -0500

    script btntop
    agregado archivo scritpt rutinas y su referencia en las páginas.

---
+++ rutinas.js
@@ -0,0 +1,27 @@
+﻿//Rutinas
+
+//***********************************
+//script para el botón "top"
+//***********************************
+var btnInterval;
+// MUESTRA EL BOTÓN CUANDO HACE SCROLL MÁS DE 20 PX
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("BtnTop").style.display = "block";
+        clearTimeout(btnInterval);
+        btnInterval = setTimeout("ocultarbtn()", 2500);
+    } else {
+        document.getElementById("BtnTop").style.display = "none";
+    }
+}
+function ocultarbtn() {
+    document.getElementById("BtnTop").style.display = "none";
+}
+
+// CUANDO EL USUARIO HACE CLICK, HACE SCROLL HASTA EL INICIO DEL DOCUMENTO
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
