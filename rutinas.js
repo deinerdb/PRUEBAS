d0d2b201789128b332ab8d4b81f546b4189851f6
Deiner Domínguez Bolaño <deiner_db@hotmail.com>
2017-07-20 19:14:58 -0500

    btntop se opaca
    el botón top se hace opaco antes de desaparecer

--- rutinas.js
+++ rutinas.js
@@ -4,20 +4,40 @@
 //script para el botón "top"
 //***********************************
 var btnInterval;
+var btnContador;
 // MUESTRA EL BOTÓN CUANDO HACE SCROLL MÁS DE 20 PX
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("BtnTop").style.opacity = "1";
         document.getElementById("BtnTop").style.display = "block";
-        clearTimeout(btnInterval);
-        btnInterval = setTimeout("ocultarbtn()", 2500);
+        clearInterval(btnInterval);
+        btnContador = 0;
+        btnInterval = setInterval("manejarbtn()", 20);
     } else {
         document.getElementById("BtnTop").style.display = "none";
     }
 }
-function ocultarbtn() {
-    document.getElementById("BtnTop").style.display = "none";
+function manejarbtn() {    
+    btnContador += 1;
+    switch (true) {
+        case btnContador <= 1:
+            //primera vez es totalmente visible
+            document.getElementById("BtnTop").style.opacity = "1";
+            break;
+        case btnContador == 125:
+            //finaliza
+            document.getElementById("BtnTop").style.display = "none";            
+            clearInterval(btnInterval);            
+            break;
+        case btnContador >= 100:
+            //lo hace cada vez más opaco
+            document.getElementById("BtnTop").style.opacity = 300/btnContador-2;
+    }
+
+    
+    
 }
 
 // CUANDO EL USUARIO HACE CLICK, HACE SCROLL HASTA EL INICIO DEL DOCUMENTO
