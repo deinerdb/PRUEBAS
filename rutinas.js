28b84818cff51373238068faa87d41a353c3bfef
Deiner Domínguez Bolaño <deiner_db@hotmail.com>
2018-08-15 17:59:48 -0500

    frases update
    snackbar agregado, disponible a todas las páginas.

--- rutinas.js
+++ rutinas.js
@@ -15,6 +15,27 @@ function imprimir() {
 function nowImprime() {
     window.print();
 }
+// PARA EL SNACKBAR
+//****************
+
+function showSnackbar(msj) {
+    // Get the snackbar DIV
+    var x = document.getElementById("snackbar");
+
+    //mensaje modificado
+    x.innerHTML = msj;
+
+    // Add the "show" class to DIV
+    x.className = "show";
+
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+}
+
+
+// FIN SNACKBAR
+
+
 //****************************************
 // COPIAR
 //****************************************
