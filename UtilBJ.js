349c34ae2d38f8a0fb396e1aaa392575b6d6de1d
Deiner Domínguez Bolaño <deiner_db@hotmail.com>
2021-06-13 17:33:23 -0500

    UTILBJ
    INICIANDO

---
+++ UtilBJ.js
@@ -0,0 +1,49 @@
+﻿//***********************************
+//script para el botón "top"
+//***********************************
+var btnInterval;
+var btnContador;
+// MUESTRA EL BOTÓN CUANDO HACE SCROLL MÁS DE 20 PX
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("BtnTop").style.opacity = "1";
+        document.getElementById("BtnTop").style.display = "block";
+        clearInterval(btnInterval);
+        btnContador = 0;
+        btnInterval = setInterval("manejarbtn()", 20);
+    } else {
+        document.getElementById("BtnTop").style.display = "none";
+    }
+}
+function manejarbtn() {
+    btnContador += 1;
+    switch (true) {
+        case btnContador <= 1:
+            //primera vez es totalmente visible
+            document.getElementById("BtnTop").style.opacity = "1";
+            break;
+        case btnContador == 125:
+            //finaliza
+            document.getElementById("BtnTop").style.display = "none";
+            clearInterval(btnInterval);
+            break;
+        case btnContador >= 100:
+            //lo hace cada vez más opaco
+            document.getElementById("BtnTop").style.opacity = 500 / btnContador - 4;
+    }
+
+
+
+}
+
+// FUNCIÓN QUE HACE SCROLL HASTA EL INICIO DEL DOCUMENTO
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+
+
+// scroll
+topFunction();
